5e16b23a2ca03792aefc1e001b7ac21d2c5fd590
Leonardo Rangel dos Santos <leonardorangel2604@gmail.com>
2020-02-27 20:19:23 -0300

    Implementing grouping search

--- SalesSystem/Controllers/SalesRecordsController.cs
+++ SalesSystem/Controllers/SalesRecordsController.cs
@@ -42,9 +42,23 @@ namespace SalesSystem.Controllers
             return View(sales);
         }
 
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+
+            ViewData["minDate"] = minDate.Value.ToString("yyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyy-MM-dd");
+
+            List<IGrouping<Department, SalesRecord>> sales = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(sales);
         }
     }
 }
--- SalesSystem/Services/SalesRecordService.cs
+++ SalesSystem/Services/SalesRecordService.cs
@@ -36,5 +36,26 @@ namespace SalesSystem.Services
                 .OrderByDescending(obj => obj.Date)
                 .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(obj => obj.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(obj => obj.Date <= maxDate);
+            }
+
+            return await result
+                .Include(obj => obj.Seller)
+                .Include(obj => obj.Seller.Department)
+                .OrderByDescending(obj => obj.Date)
+                .GroupBy(obj => obj.Seller.Department)
+                .ToListAsync();
+        }
     }
 }
