9ce1f2d2d302385dabba9733cb9b5e018aa5899f
Leonardo Rangel dos Santos <leonardorangel2604@gmail.com>
2020-02-27 11:22:59 -0300

    CRUD scaffolding

--- SalesSystem/Startup.cs
+++ SalesSystem/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesSystem.Data;
 
 namespace SalesSystem
 {
@@ -33,6 +35,9 @@ namespace SalesSystem
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesSystemContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesSystemContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
