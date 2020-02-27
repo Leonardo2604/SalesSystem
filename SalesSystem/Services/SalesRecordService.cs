9965d66666b3eb2198840577e107f9fa9854571e
Leonardo Rangel dos Santos <leonardorangel2604@gmail.com>
2020-02-27 20:05:35 -0300

    Implementing simple search

---
+++ SalesSystem/Services/SalesRecordService.cs
@@ -0,0 +1,40 @@
+﻿using SalesSystem.Data;
+using SalesSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesSystem.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesSystemContext _context;
+
+        public SalesRecordService(SalesSystemContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(obj => obj.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(obj => obj.Date <= maxDate);
+            }
+
+            return await result
+                .Include(obj => obj.Seller)
+                .Include(obj => obj.Seller.Department)
+                .OrderByDescending(obj => obj.Date)
+                .ToListAsync();
+        }
+    }
+}
