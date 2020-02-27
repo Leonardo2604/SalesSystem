5c97bf4ba63deb5c15616dfaa997869fbd078023
Leonardo Rangel dos Santos <leonardorangel2604@gmail.com>
2020-02-27 13:46:33 -0300

    Seeding service

--- SalesSystem/Startup.cs
+++ SalesSystem/Startup.cs
@@ -39,14 +39,17 @@ namespace SalesSystem
             services.AddDbContext<SalesSystemContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesSystemContext"), builder => 
                         builder.MigrationsAssembly("SalesSystem")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
